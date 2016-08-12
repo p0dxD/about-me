9fa587b399ab6526d9e37341d0e66b9500f623cd
Jose Rodriguez <jose0797@gmail.com>
2016-08-12 16:27:51 -0400

    not sure

---
+++ src/assets/js/profile.js
@@ -0,0 +1,7 @@
+//parallex effect on background
+var scene = document.getElementById('scene');
+var parallax = new Parallax(scene);
+
+//for the background
+$('body').shards([0,0,0,.5],[239,250,180,.2],[0,0,0,.1],10,3,2,.8,false);
+
