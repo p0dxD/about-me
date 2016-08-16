4ba5a448a45676784a8a03e21591c886d2836dd0
Jose Rodriguez <jose0797@gmail.com>
2016-08-16 17:51:44 -0400

    modified center look

--- src/assets/js/profile.js
+++ src/assets/js/profile.js
@@ -3,7 +3,7 @@ var scene = document.getElementById('scene');
 var parallax = new Parallax(scene);
 
 //for the background
-$('body').shards([0,0,0,.5],[239,250,180,.2],[0,0,0,.1],10,3,2,.8,false);
+$('body').shards([0,0,0,.5],[239,250,180,.2],[0,0,0,.1],20,1.0,0,.1,false);
 
 
 // $(window).scroll(function() {
